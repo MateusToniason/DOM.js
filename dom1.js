fa4b1d370a5dd6ab740db5dcc695d024c1939e2d
Mateus <108558575+MateusToniason@users.noreply.github.com>
2023-01-06 17:25:45 -0300

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,14 @@
+/*todos os comentários abaixo 
+são testes que eu fiz*/
+
+//1//
+/*var x = document.querySelector('div#msg');
+x.style.color = '#419ecc';*/
+
+//2//
+/*var y = document.querySelector('h1');
+y.style.color = 'red';*/
+
+//3//
+/*var w = document.querySelector('p.p1');
+w.style.background = 'purple';*/
